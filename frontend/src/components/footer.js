7499aa8c51ff8b93adf378fe468f2e9e1a4086db
Rijikashi <isaacjfu@gmail.com>
2025-01-09 16:44:52 -0800

    updating footer to include 5 column menus

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -9,29 +9,14 @@ import wechat from '../resources/icon-wechat.png'
 import x from '../resources/icon-x.png'
 import youtube from '../resources/icon-youtube.png'
 
-const Footer = ( {} ) => {
-  let navigate = useNavigate()
-
-  const handleAboutClick = () => {
-    navigate('/about')
-  }
-  const handleLogoClick = () => {
-    navigate('/')
-  }
-  const handleAccountClick = () => {
-    navigate('/account')
-  }
+const FooterTop = ( {handleLogoClick} ) => {
   return (
-    <footer class='footer'>
-      <div class = 'footer-menu'>
-        <span class = 'span-button' onClick = {handleAboutClick}> About </span>
-        <img
-            src={logo}
-            class = 'footer-logo-img'
-            onClick={handleLogoClick}
-        />
-        <span class = 'span-button' onClick ={handleAccountClick}> Account </span>
-      </div>
+    <div class = 'footer-top'> 
+      <img
+          src={logo}
+          class = 'footer-logo-img'
+          onClick={handleLogoClick}
+      />
       <div class = 'footer-icons'>
         <img
           src={fb}
@@ -58,6 +43,57 @@ const Footer = ( {} ) => {
           class = 'footer-icon-img'
         />
       </div>
+    </div>
+  )
+}
+const FooterBottom = ( {}) => {
+  const menus =[
+    {header: 'Models', content: ['Commendatore', 'Imperator', 'Silver Arrow', 'Erator', 'Spyder', 'Autobahnkurier']},
+    {header: 'Loren', content: ['Duis', 'Consectetur', 'Labore', 'Excepteur']},
+    {header: 'About Us', content: ['News', 'Investors', 'Our Vision', 'Our Story', 'Technology']},
+    {header: 'Corporate', content: ['Contact Us', 'Careers', 'Offices']},
+    {header: 'Loren', content: ['Duis', 'Consectetur', 'Labore', 'Excepteur']},
+  ]
+  return(
+    <div class = 'footer-bottom'>
+      {menus.map((menu,index) => (
+        <FooterBottomMenu menu = {menu}/>
+      ))}
+    </div>
+  )
+}
+const FooterBottomMenu = ({menu}) => {
+
+  return (
+    <div class = 'footer-bottom-menu'>
+        <h2 class = 'span-button'> {menu.header}</h2>
+        <div class = 'footer-bottom-menu-container'> 
+          {menu.content.map((c,index) => (
+            <span class ='span-button'>{c}</span>
+          ))}
+        </div>
+
+    </div>
+  )
+}
+const Footer = ( {} ) => {
+  let navigate = useNavigate()
+
+  const handleAboutClick = () => {
+    navigate('/about')
+  }
+  const handleLogoClick = () => {
+    navigate('/')
+  }
+  const handleAccountClick = () => {
+    navigate('/account')
+  }
+  // <span class = 'span-button' onClick ={handleAccountClick}> Account </span>
+  // <span class = 'span-button' onClick = {handleAboutClick}> About </span>
+  return (
+    <footer class='footer'>
+      <FooterTop handleLogoClick= {handleLogoClick}/>
+      <FooterBottom />
       <div class="footer-copyright">© Isdera 2025</div>
     </footer>
   )
